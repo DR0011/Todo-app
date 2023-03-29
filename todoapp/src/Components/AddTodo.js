6dfebc87835290654d4e0759ef9c077eda963ea2
Darshan <pateldarshan0011@gmail.com>
2023-03-29 10:22:30 -0500

    create redux-toolkit

--- todoapp/src/Components/AddTodo.js
+++ todoapp/src/Components/AddTodo.js
@@ -1,90 +1,83 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addNewTodo, updateTodo } from "../redux/actions";
+import { addtodo, editTodo, deleteTodo } from '../store/TodosSlice'
 
 
 export const AddTodo = () => {
-    const [value, setValue] = useState({});
-    const [error, setError] = useState("");
-    const dispatch = useDispatch();
-    const isEdit = useSelector((state) => state.todoReducer.isEdit);
-    const editTodo = useSelector((state) => state.todoReducer.editTodo);
 
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+    const todos = useSelector((state) => state.todos.items)
 
-    useEffect(() => {
-        editTodo && setValue(() => editTodo);
-    }, [editTodo]);
+    const dispatch = useDispatch()
 
-    const onSubmit = (e) => {
-        e.preventDefault();
-
-        if (!value?.title) {
-            setError((error) => ({
-                ...error,
-                title: 'Please enter todo title',
-            }));
-        }
-        if (!value?.description) {
-            setError((error) => ({
-                ...error,
-                description: 'Please enter todo description'
-            }));
-        }
-        if (isEdit) {
-            dispatch(updateTodo(editTodo.id, value));
-        }
-        else if (value?.title && value?.description) {
-            dispatch(addNewTodo(value));
-        }
-        document.getElementById("todoForm").reset();
-
-    };
-    const changeEvent = (e) => {
-        setValue(
-            {
-                ...value,
-                [e.target.name]: e.target.value,
-            },
-        );
-
-    };
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        dispatch(addtodo({ id: todos.length + 1, title, description }))
+        setTitle("");
+        setDescription("")
+    }
 
     return (
         <div className="container my-4 py-1 border">
-            <form className="mt-3 mb-2" id="todoForm" onSubmit={onSubmit} >
+            <form className="mt-3 mb-2" id="todoForm" onSubmit={handleSubmit}>
                 <div className="row">
                     <div className="col-xl-3">
                         <label className="sr-only">Name</label>
                         <input
                             type="text"
-                            name="title"
+                            value={title}
                             className="form-control mb-2 mr-sm-3"
                             placeholder="Todo Title"
-                            defaultValue={value?.title}
-                            onChange={(e) => changeEvent(e)}
+                            onChange={(e) => setTitle(e.target.value)}
                         />
-                        <span className="text-danger">{error?.title}</span>
+
                     </div>
 
                     <div className="col-xl-3">
                         <label className="sr-only">Description</label>
                         <input
                             type="text"
-                            name="description"
+                            value={description}
                             className="form-control mb-2 mr-sm-3"
                             placeholder="Description"
-                            defaultValue={value?.description}
-                            onChange={(e) => changeEvent(e)}
-
+                            onChange={(e) => setDescription(e.target.value)}
                         />
-                        <span className="text-danger">{error?.description}</span>
+
                     </div>
 
                     <div className="col-xl-2">
-                        <button className="btn btn-primary mb-2 mt-4" type="submit">{isEdit ? 'Update Todo' : 'Create Todo'}</button>
+
+                        <button className="btn btn-primary mb-2 mt-4" onClick={handleSubmit}>Add Todo</button>
                     </div>
                 </div>
             </form>
+            <div className="todos">
+                {todos.length > 0 ? todos.map(todo => <div className='post'>
+                    <h2>{todo.title}</h2>
+                    <p>{todo.description}</p>
+
+                    <button
+                        className="btn btn-primary btn-sm"
+                        onClick={() => {
+
+                            dispatch(editTodo({ id: todo.id, title: "New Title", description: "New Description" }));
+                        }}>
+                        Edit
+                    </button>
+                    <button
+                        className="btn btn-danger btn-sm"
+                        onClick={() => {
+
+                            dispatch(deleteTodo({ id: todo.id }));
+                        }}>
+                        Delete
+                    </button>
+                </div>) : 'There is no todo'}
+            </div>
         </div>
+
     );
 };
+
+
